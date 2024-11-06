ee5b9f674baa1ccaef9d0475f7dbdb9aad19d5b9
Richard T. Carback III <rick.carback@gmail.com>
2024-11-06 00:16:32 -0500

    bump to 10s

--- mautrix_telegram/abstract_user.py
+++ mautrix_telegram/abstract_user.py
@@ -756,7 +756,7 @@ class AbstractUser(ABC):
 
         try:
             self.log.warning("Message %d sleeping", update.id)
-            time.sleep(5)
+            time.sleep(10)
             msgs = await self.client.get_messages(portal.peer, ids=[update.id])
             if len(msgs) == 0:
                 self.log.warning("Message %d not found", update.id)
